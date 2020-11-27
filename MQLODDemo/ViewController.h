8a9fe06671d35af8fa052e9c132ffaa5df13b627
MQTwsit <mq943691300@163.com>
2020-11-27 10:48:27 +0800

    Initial Commit

---
+++ MQLODDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  MQLODDemo
+//
+//  Created by ma qi on 2020/11/27.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MQLODDemo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  MQLODDemo
+//
+//  Created by ma qi on 2020/11/27.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
